8368b90ca4baa0cdd631b23e4c54dca8b1b08d4c
rescarabel0 <r.escarabelo@gmail.com>
2021-04-20 09:57:54 -0300

    struct prontas

--- trabParte1.C
+++ trabParte1.C
@@ -1,6 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct musica {
+    int id;
+    char titulo[200];
+    char artista[200];
+    char album[200];
+    int duracao; //em segundos
+} musica;
+
+typedef struct musica_no {
+    musica *musica;
+    struct musica_no *ant;
+    struct musica_no *prox;
+} musica_no;
+
+typedef struct playlist_no {
+    musica *musica;
+    struct playlist_no *prox;
+} playlist_no;
+
 int main(){
-    
+
 }
