0e90d4560e6b6daa1abfc4e145f4af9802211475
rescarabel0 <r.escarabelo@gmail.com>
2021-04-20 10:06:56 -0300

    structs

--- trabParte1.C
+++ trabParte1.C
@@ -20,6 +20,13 @@ typedef struct playlist_no {
     struct playlist_no *prox;
 } playlist_no;
 
+typedef struct lplaylists_no {
+    int id;
+    char nome[200];
+    playlist_no *musicas;
+    struct lplaylists_no *prox;
+} lplaylists_no;
+
 int main(){
 
 }
